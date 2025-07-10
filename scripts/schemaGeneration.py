397f6781fe66ff12ce4c1008e8efe7c3c9df7a57
ThiagoChafado <thiagochafado123@gmail.com>
2025-07-10 11:04:52 -0300

    finished schema generation

---
+++ scripts/schemaGeneration.py
@@ -0,0 +1,51 @@
+import os
+import json
+from genson import SchemaBuilder
+
+#Path to folders
+rawJsonDir = "../datasets/rawJson"
+processedSchemasDir = "../datasets/processedSchemas"
+
+def generate_schema():
+    
+    """ Reads all the files in the rawJsonDir and 
+        generates a schema for each file
+        then saves the schema in the processedSchemasDir"""
+        
+        
+    print("Generating schemas...")
+    
+    if not os.path.exists(processedSchemasDir):
+        os.makedirs(processedSchemasDir)
+        print(f"Created directory: {processedSchemasDir}")
+    jsonFiles = [f for f in os.listdir(rawJsonDir) if f.endswith('.json')]
+    if not jsonFiles:
+        print("No JSON files found in the rawJsonDir.")
+        return
+    
+    print(f"Found {len(jsonFiles)} JSON files in {rawJsonDir}.")
+    
+    for jsonFile in jsonFiles:
+        entryPath = os.path.join(rawJsonDir, jsonFile)
+        outputName = jsonFile.replace('.json', '.schema.json')
+        outputPath = os.path.join(processedSchemasDir, outputName)
+        
+        try:
+            with open(entryPath, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                
+                builder = SchemaBuilder()
+                builder.add_object(data)
+                schema = builder.to_schema()
+            
+            with open(outputPath, 'w',encoding='utf-8') as outputFile:
+                json.dump(schema, outputFile, indent=4)
+                
+            print(f"Schema generated and saved to {outputPath}")
+    
+        except Exception as e:
+            print(f"An error occurred while processing {entryPath}: {e}")
+    
+if __name__ == "__main__":
+    generate_schema()
+    print("Schema generation completed.")
